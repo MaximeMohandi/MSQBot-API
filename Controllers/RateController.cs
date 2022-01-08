964fc636f33a1180cac661c97cd19c3ebdfa35a2
Maxime Mohandi <mohandi.maxime@gmail.com>
2022-01-08 04:58:29 +0100

    <feat> add rate controller

---
+++ Controllers/RateController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSQBot_API.Entities.DTOs;
+using MSQBot_API.Services;
+
+namespace MSQBot_API.Controllers
+{
+    [Route("api/rates")]
+    [ApiController]
+    public class RateController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly RateServices _rateServices;
+
+        public RateController(ILogger<RateController> logger, RateServices rateServices)
+        {
+            _logger = logger;
+            _rateServices = rateServices;
+        }
+
+        [HttpGet("{userId:long}", Name = "user")]
+        public IActionResult Get(long userId)
+        {
+            try
+            {
+                var rates = _rateServices.GetRatesUser(userId);
+
+                if (rates == null) return NotFound();
+
+                return Ok(rates);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(500, "Internal server error : Something went wrong when fetching datas for user " + userId);
+            }
+        }
+    }
+}
---
+++ Services/RateServices.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MSQBot_API.Entities.DTOs;
+using MSQBot_API.Entities.Models;
+using MSQBot_API.Helpers;
+
+namespace MSQBot_API.Services
+{
+    public class RateServices
+    {
+        private readonly MSQBotDbContext _dbContext;
+
+        public RateServices(MSQBotDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// Get all the rate a user had given to a movie.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>List of rates with movie associated for a user</returns>
+        public List<RatesMovieDto> GetRatesUser(long userId)
+        {
+            return _dbContext.Rates
+                    .Include(r => r.Movie)
+                    .Include(r => r.User)
+                    .Where(r => r.UserId == userId)
+                    .ToList()
+                    .MapRatesToDTOs();
+        }
+    }
+}
